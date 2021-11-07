97c9bf0618e001675feeef7588f757dd664ddeb1
Fedor Masor <masor.dev@gmail.com>
2021-11-07 22:11:45 +0300

    get data testing

--- app/src/main/java/com/masorone/cryptocompare/MainActivity.kt
+++ app/src/main/java/com/masorone/cryptocompare/MainActivity.kt
@@ -2,10 +2,55 @@ package com.masorone.cryptocompare
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.masorone.cryptocompare.api.ApiFactory
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
+
+    private val compositeDisposable = CompositeDisposable()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        getTopCoinsInfoInLog()
+        getFullPriceListInLog()
+    }
+
+    private fun getTopCoinsInfoInLog() {
+        compositeDisposable.add(
+            ApiFactory.apiService.getTopCoinsInfo()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                    Log.d(TAG, "CoinInfoListOfData -> $it")
+                }, {
+                    Log.d(TAG, "Error(CoinInfoListOfData) -> $it")
+                })
+        )
+    }
+
+    private fun getFullPriceListInLog() {
+        compositeDisposable.add(
+            ApiFactory.apiService.getFullPriceList()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                    Log.d(TAG, "CoinPriceInfoRawData -> $it")
+                }, {
+                    Log.d(TAG, "Error(CoinPriceInfoRawData) -> $it")
+                })
+        )
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        compositeDisposable.dispose()
     }
 }
