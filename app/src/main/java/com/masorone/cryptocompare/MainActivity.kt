0205a836c043fac5772ac80fdc88b63f5d176fbb
Fedor Masor <masor.dev@gmail.com>
2021-11-08 17:49:53 +0300

    get data from vm

--- app/src/main/java/com/masorone/cryptocompare/MainActivity.kt
+++ app/src/main/java/com/masorone/cryptocompare/MainActivity.kt
@@ -1,56 +1,32 @@
 package com.masorone.cryptocompare
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import com.masorone.cryptocompare.api.ApiFactory
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.schedulers.Schedulers
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
 
-    private val compositeDisposable = CompositeDisposable()
+    private lateinit var vm: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        getTopCoinsInfoInLog()
-        getFullPriceListInLog()
+        initViewModel()
+        loadData()
     }
 
-    private fun getTopCoinsInfoInLog() {
-        compositeDisposable.add(
-            ApiFactory.apiService.getTopCoinsInfo()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe({
-                    Log.d(TAG, "CoinInfoListOfData -> $it")
-                }, {
-                    Log.d(TAG, "Error(CoinInfoListOfData) -> $it")
-                })
-        )
+    private fun initViewModel() {
+        vm = ViewModelProvider(
+            this,
+            MainViewModelFactory(context = applicationContext)
+        )[MainViewModel::class.java]
     }
 
-    private fun getFullPriceListInLog() {
-        compositeDisposable.add(
-            ApiFactory.apiService.getFullPriceList()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe({
-                    Log.d(TAG, "CoinPriceInfoRawData -> $it")
-                }, {
-                    Log.d(TAG, "Error(CoinPriceInfoRawData) -> $it")
-                })
-        )
+    private fun loadData() {
+        vm.getTopCoinsInfo()
     }
 
     companion object {
         private const val TAG = "MainActivity"
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        compositeDisposable.dispose()
     }
 }
