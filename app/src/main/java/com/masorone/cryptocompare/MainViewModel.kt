9e0ec37d2d153a98d18b2eb2566d7dd3fca7d63f
Fedor Masor <masor.dev@gmail.com>
2021-11-08 20:38:08 +0300

    add getDetailInfo() & getPriceListFromRawData()

--- app/src/main/java/com/masorone/cryptocompare/MainViewModel.kt
+++ app/src/main/java/com/masorone/cryptocompare/MainViewModel.kt
@@ -2,12 +2,16 @@ package com.masorone.cryptocompare
 
 import android.content.Context
 import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import com.google.gson.Gson
 import com.masorone.cryptocompare.api.ApiFactory
 import com.masorone.cryptocompare.db.CoinDatabase
-import io.reactivex.android.schedulers.AndroidSchedulers
+import com.masorone.cryptocompare.pojo.CoinPriceInfo
+import com.masorone.cryptocompare.pojo.CoinPriceInfoRawData
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
 
 class MainViewModel(context: Context) : ViewModel() {
 
@@ -15,21 +19,50 @@ class MainViewModel(context: Context) : ViewModel() {
     private val db = CoinDatabase.getInstance(context)
     val priceList = db.coinPriceInfoDao().getPriceList()
 
-    fun loadData() {
+    init {
+        loadData()
+    }
+
+    fun getDetailInfo(fSym: String): LiveData<CoinPriceInfo> {
+        return db.coinPriceInfoDao().getPriceInfoAboutCoin(fSym = fSym)
+    }
+
+    private fun loadData() {
         compositeDisposable.add(
             ApiFactory.apiService.getTopCoinsInfo()
                 .map { it.data?.map { datum -> datum.coinInfo?.name }?.joinToString(",") }
                 .flatMap { ApiFactory.apiService.getFullPriceList(fSyms = it) }
+                .map { getPriceListFromRawData(it) }
+                .delaySubscription(10, TimeUnit.SECONDS)
+                .repeat()
+                .retry()
                 .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
                 .subscribe({
-
-
-                    Log.d(TAG, "CoinPriceInfoRawData -> $it")
+                    db.coinPriceInfoDao().insertPriceList(it)
                 }, {
-                    Log.d(TAG, "Error(CoinPriceInfoRawData) -> $it")
+                    Log.d(TAG, "Error: loadData() -> $it")
                 })
         )
+    }
+
+    private fun getPriceListFromRawData(
+        coinPriceInfoRawData: CoinPriceInfoRawData
+    ): List<CoinPriceInfo> {
+        val result = ArrayList<CoinPriceInfo>()
+        val jsonObject = coinPriceInfoRawData.coinPriceInfoJsonObject ?: return result
+        val coinKeySet = jsonObject.keySet()
+        for (coinKey in coinKeySet) {
+            val currencyJson = jsonObject.getAsJsonObject(coinKey)
+            val currencyKeySet = currencyJson.keySet()
+            for (currencyKey in currencyKeySet) {
+                val priceInfo = Gson().fromJson(
+                    currencyJson.getAsJsonObject(currencyKey),
+                    CoinPriceInfo::class.java
+                )
+                result.add(priceInfo)
+            }
+        }
+        return result
     }
 
     companion object {
